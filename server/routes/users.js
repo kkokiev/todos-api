86a3ba537a40e041e97247c5767dfc716dee3872
Constantine <constantine.kononenko@gmail.com>
2018-09-20 13:49:52 +0300

    Add POST /user/login route

--- server/routes/users.js
+++ server/routes/users.js
@@ -26,4 +26,19 @@ router.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+router.post('/users/login', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+
+  User
+    .findByCredentials(body.email, body.password)
+    .then((user) => {
+      user.generateAuthToken().then((token) => {
+        res.header('x-auth', token).send(user);
+      });
+    })
+    .catch((err) => {
+      res.status(400).send();
+    });
+});
+
 module.exports = router;
